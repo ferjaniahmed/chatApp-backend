61cce87a7e891d50774aef7fed0e120bde2c673a
ferjahm <ferjani.ahmed.2001@gmail.com>
2024-03-03 20:29:06 +0100

    create connection to database*

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule , 
+    MongooseModule.forRoot('mongodb://localhost/chatApp'),
+    ConfigModule.forRoot()
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
