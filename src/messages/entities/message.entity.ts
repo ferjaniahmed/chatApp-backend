0bda368fbb0e446916aa16cf17e9d9c97409ff3b
ferjahm <ferjani.ahmed.2001@gmail.com>
2024-03-07 14:06:35 +0100

    implement the message module

---
+++ src/messages/entities/message.entity.ts
@@ -0,0 +1,8 @@
+import { UserEntity } from "src/users/entities/user.entity"
+
+export class MessageEntity {
+    _id : string
+    sender: UserEntity
+    receiver : UserEntity
+    data : string
+}
